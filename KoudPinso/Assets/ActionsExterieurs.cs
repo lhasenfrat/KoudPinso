feed460f4ef9873a9c8ebaf4f933059b5461ebd4
Sebastien Goll <sebastiengoll@hotmail.fr>
2022-04-13 10:06:49 +0200

    UI sur la tarte

---
+++ KoudPinso/Assets/ActionsExterieurs.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionsExterieurs : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void revoirIntro(){
+        Debug.Log("Cinematique");
+    }
+
+    public void reinitialiser(){
+        Debug.Log("Effaçage");
+    }
+
+    public void quitter(){
+        Debug.Log("Quittage");
+    }
+}
