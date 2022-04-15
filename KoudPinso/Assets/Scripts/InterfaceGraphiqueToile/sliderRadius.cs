ae21ee02a1b7ee185a025b63d12a75312288ca77
Florierie <villegasfloriepro@gmail.com>
2022-04-15 09:23:44 +0200

    menu outil avec slider

---
+++ KoudPinso/Assets/Scripts/InterfaceGraphiqueToile/sliderRadius.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class sliderRadius : MonoBehaviour
+{
+
+    public GameObject slider;
+    public GameObject circle;
+    float EpaisseurSlider;
+    public float defaultRadius;
+    RectTransform rectTransform;
+
+
+    void Start()
+    {
+        EpaisseurSlider = slider.GetComponent <Slider> ().value;
+        EpaisseurSlider = 0.5f;
+
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnValueChanged(float newValue)
+    {
+        Debug.Log(newValue);
+        rectTransform = circle.GetComponent<RectTransform>();
+        Vector3 newSize = new Vector3 (newValue*defaultRadius, newValue*defaultRadius,0 );
+        rectTransform.DOScale(newSize, 0f);
+    }
+}
