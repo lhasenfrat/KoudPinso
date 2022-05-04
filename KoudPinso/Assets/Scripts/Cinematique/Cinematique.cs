6d42f05380e7f6f8c3d672537d67358fd913c326
Florierie <villegasfloriepro@gmail.com>
2022-05-04 11:26:00 +0200

    boutons fini et revoir cinematique inactifs quand dessin libre

--- KoudPinso/Assets/Scripts/Cinematique/Cinematique.cs
+++ KoudPinso/Assets/Scripts/Cinematique/Cinematique.cs
@@ -13,6 +13,9 @@ public class Cinematique : MonoBehaviour
     public GameObject exo;
 
     private string titreExo;
+
+    public GameObject boutonCinematique; 
+	public GameObject boutonFini;
     // Start is called before the first frame update
     public void checkDessinLibre()
     {
@@ -23,11 +26,15 @@ public class Cinematique : MonoBehaviour
         {
             panelCinematique.SetActive(true);
             toile.GetComponent<Drawable>().setAllowedDrawing(false);
+            boutonCinematique.SetActive(true);
+            boutonFini.SetActive(true);
         }else
         {
             
             panelCinematique.SetActive(false);
             toile.GetComponent<Drawable>().setAllowedDrawing(true);
+            boutonCinematique.SetActive(false);
+            boutonFini.SetActive(false);
         }
 
     }
